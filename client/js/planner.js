bfdc0fb3faf8122c3d09d1a3c82b5bea26cc049e
shanjing012 <33203151+shanjing012@users.noreply.github.com>
2018-06-20 20:56:18 +0800

    Working on saving complete the getting of trip from db/new/localstorage

--- client/js/planner.js
+++ client/js/planner.js
@@ -1,31 +1,80 @@
 import { ReactiveVar } from 'meteor/reactive-var'
 import { Trips } from '../../lib/models/db';
 import { Location } from '../../lib/models/db';
+import { Country } from '../../lib/models/db';
 
 
 Template.planner.onCreated(function() {
-	Meteor.subscribe('trips');
+	var subscription = Meteor.subscribe('trips');
+	Meteor.subscribe('getCountry');
 
+	//change this to session variable later. (tested session variable, abit iffy)
 	this.trip = new ReactiveVar();
+	var trip = this.trip;
+	
+	//eg /?_id=new&country=mexico, _id = new, country = mexico
+	if(FlowRouter.getQueryParam('_id') == "new")
+	{
+		//new trip
+		console.log(Meteor.userId());
+		let countryEntered = FlowRouter.getQueryParam('country');
+		if(countryEntered == undefined)
+			countryEntered = "Singapore";
+		let newTrip = {
+			owner: Meteor.userId(),
+			country: countryEntered,
+			startDate: "",
+			dayArray: [
+				[],
+			],
+		};
+		//Session.set('trip', thistrip);
+		trip.set(newTrip);
+	}
+	else if(FlowRouter.getQueryParam('_id') == undefined)
+	{
+		//current existing trip being edited
+		//Session.set('trip', JSON.parse(localStorage.getItem('trip')));
+		trip.set(JSON.parse(localStorage.getItem('trip')));
+	}
+	else
+	{
+		//load a trip from db
+		this.autorun(function(autorunner) {
+			//not yet subscribed, return
+			if(! subscription.ready())
+			  return;
+			//else do this
+			else
+			{
+				var queryTrip = Trips.findOne( { _id: FlowRouter.getQueryParam('_id') } );//.fetch()[0];
+				//console.log(trip);
+				//if exist = not undefined
+				if(queryTrip != undefined)
+					//Session.set('trip', queryTrip);
+					trip.set(queryTrip);
+				else
+					console.log("invalid");
+				autorunner.stop();
+			}
+		});
+	}
+
+});
+
+Template.planner.onDestroyed(function (){
+
+	//save as local storage
+	localStorage.setItem('trip', JSON.stringify(this.trip.get()));
+
 });
 
 Template.planner.helpers({
 
-	//get trip from this method and set to local variable (only done when needed to get a saved trip)
-	getTrip: function() {
-		Template.instance().trip.set(Trips.find( { _id: "sfasdfa" } ).fetch()[0]);
-		if(Template.instance().trip.get() != undefined)
-		{
-			console.log(Template.instance().trip.get());
-		}
-		else
-			console.log("Cant find trip");
-		//return trip;
-		//return Trips.find( { _id: "sfasdfa" } ).fetch();
-	},
 	//check trip
 	haveTrip: function() {
-		if(Template.instance().trip.get() == undefined)
+		var trip = Template.instance().trip.get();
+		if(trip == undefined)
 		{
 			//console.log("no trip yet");
 			return false;
@@ -36,30 +85,22 @@ Template.planner.helpers({
 			return true;
 		}
 	},
-
-	//for creating new trips with country entered
-	createTrip: function(countryEntered) {
-		trip = {
-			country: countryEntered,
-			startDate: "",
-			dayArray: [
-				[ "mountain" , "beach" , "forest", "forest", "forest"],
-				[ "forest" , "beach" ]
-			],
-		}
-	},
 	
 	//get trip will have gotten the trip, so return the trip array
 	tripDays() {
+		console.log(Template.instance().trip.get());
 		return Template.instance().trip.get().dayArray;
+		//Session.get('trip').dayArray;
 	},
 
 	tripCountry: function() {
 		return Template.instance().trip.get().country;
+		//Session.get('trip').country;
 	},
 
 	tripStartDate: function() {
 		return Template.instance().trip.get().startDate;
+		//Session.get('trip').startDate;
 	},
 
 	/*
@@ -84,11 +125,14 @@ Template.planner.helpers({
 		placeId: asdasd
 	}
 	*/
-
-	//set this index to ind+1
 	
+	checkLoginAndData: function() {
+		var value = (Template.instance().subscriptionsReady() && Meteor.userId() != null) 
+		return value;
+	},
 
 	reactiveTrip: function() {
+		//pass on reactive trip to children templates
 		return Template.instance().trip;
 	}
 });
@@ -104,6 +148,23 @@ Template.planner.events({
 		Template.instance().trip.set(Template.instance().trip.get());
 		//close(save) a javascript modal thing
 		//gotten from bootstrap https://getbootstrap.com/docs/4.0/components/modal/?#varying-modal-content
+	},
+
+	'click .btn-saveTrip' (event) {
+
+		//if there is existing, update
+		//else add new entry
+		var trip = Template.instance().trip.get();
+		var queryTrip = Trips.findOne( { _id: Template.instance().trip.get()._id } );
+		if(queryTrip == undefined)
+		{
+			//create new
+			Meteor.call('trips.add', trip);
+		}
+		else
+		{
+			//update existing
+		}
 	}
 });
 
@@ -113,14 +174,12 @@ Template.dayTemplate.helpers({
 		return ind+1;
 	},
 
-	
-
 });
 
 Template.dayTemplate.events({
 	'click .btn-dayAddLoc' (event) {
 		//add a blank location.
-		this.trip.get().dayArray[this.dayIndex].push("NAME OF LOCATION");
+		this.trip.get().dayArray[this.dayIndex].push("Name");
 		this.trip.set(this.trip.get());
 	}
 });
@@ -141,7 +200,6 @@ Template.locationTemplate.helpers({
 
 Template.locationTemplate.events({
 	'click .btn-deleteLoc' (event) {
-		console.log(this.dayIndex + " " + this.locIndex);
 		//remove from object.
 		this.trip.get().dayArray[this.dayIndex].splice(this.locIndex, 1);
 		this.trip.set(this.trip.get());
