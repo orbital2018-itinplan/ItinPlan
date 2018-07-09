04b2979546eee22c2f57571242185f32d2ba8dbd
shanjing <shanjing012@gmail.com>
2018-07-10 01:24:24 +0800

    some accounts stuff?..

--- client/js/accounts.js
+++ client/js/accounts.js
@@ -1,13 +1,40 @@
 Template.loginNav.helpers({
-	username() {
-		return Meteor.user().emails[0].address;
-	}
+	username: function() {
+		//return Meteor.user().emails[0].address;
+		return Meteor.user().username;
+	},
 });
 
 Template.loginNav.events({
-	'click .logout': ()=> {
+	'click #ddl-logout': ()=> {
 		//get email
-		console.log("logging out of user: " + Meteor.user().emails[0].address);
+		console.log("logging out of user: " + Meteor.user().username);
 		AccountsTemplates.logout();
-	}
+		AccountsTemplates.setState("signIn");
+	},
+
+	'click #ddl-ViewDetails'(event) {
+		FlowRouter.go('accounts');
+		AccountsTemplates.setState("signIn");
+	},
+
+	'click #ddl-ResetPassword'(event) {
+		FlowRouter.go('accounts');
+		AccountsTemplates.setState("changePwd");
+	},
+});
+
+Template.accountTemplate.helpers({
+	detailsShow: function() {
+		if(AccountsTemplates.getState() === "signIn")
+			return true;
+		else
+			return false;
+	},
+	accountUsername: function() {
+		return Meteor.user().username;
+	},
+	accountEmail: function() {
+		return Meteor.user().emails[0].address;
+	},
 });
